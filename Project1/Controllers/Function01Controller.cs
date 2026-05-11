2ea0e172a2c917b6b26d44184c4ad35cd827314e
Yoshitomo <yoshi_s51@yahoo.co.jp>
2026-05-11 17:30:25 +0900

    commit

---
+++ Project1/Controllers/Function01Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Function01Controller : ControllerBase
+    {
+        [HttpGet("{input}")]
+        public string Get(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
